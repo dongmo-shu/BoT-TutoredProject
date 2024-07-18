f9d7150acf9dd3d8b71d55a83bbf85e4b116fe03
dongmo-shu <singraft14@gmail.com>
2024-07-19 00:54:25 +0100

    User setting spages complete

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,8 +1,1 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    const mobileMenu = document.getElementById('mobile-menu');
-    if (mobileMenu.style.display === 'block') {
-        mobileMenu.style.display = 'none';
-    } else {
-        mobileMenu.style.display = 'block';
-    }
-});
+
